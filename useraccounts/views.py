c574eae3dc0d433691210d76900cc845852778df
rju <rju@purdue.edu>
2017-11-29 11:49:08 -0500

    testing separate html files

--- useraccounts/views.py
+++ useraccounts/views.py
@@ -4,7 +4,15 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.template import loader
+
 # Create your views here.
 def hello(request):
-    text = """<h1>This is our Web App Project (Lab 6) for CS 252 at Purdue University. Enjoy :)</hi>"""
-    return HttpResponse(text)
+#    text = """<h1>This is our Web App Project (Lab 6) for CS 252 at Purdue University. Enjoy :)</hi>"""
+#    return HttpResponse(text)
+    template = loader.get_template('useraccounts/index.html')
+    context = {
+        'hello': "hello there ^_^, please login",
+    }
+    return HttpResponse(template.render(context, request))
+
