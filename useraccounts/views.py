b198a10f42e69420d7b90060e0d8afe66466ec11
rju <rju@purdue.edu>
2017-12-04 20:18:58 -0500

    redirect for create/loginworks properly

--- useraccounts/views.py
+++ useraccounts/views.py
@@ -7,10 +7,24 @@ from django.http import HttpResponse
 from django.template import loader
 
 # Create your views here.
-def hello(request):
+def index(request):
 #    text = """<h1>This is our Web App Project (Lab 6) for CS 252 at Purdue University. Enjoy :)</hi>"""
 #    return HttpResponse(text)
     template = loader.get_template('useraccounts/index.html')
+    context = {
+        'hello': "hello there ^_^, please select your option",
+    }
+    return HttpResponse(template.render(context, request))
+
+def create(request):
+    template = loader.get_template('useraccounts/create.html')
+    context = {
+        'hello': "hello there ^_^, please create your account",
+    }
+    return HttpResponse(template.render(context, request))
+
+def login(request):
+    template = loader.get_template('useraccounts/login.html')
     context = {
         'hello': "hello there ^_^, please login",
     }
