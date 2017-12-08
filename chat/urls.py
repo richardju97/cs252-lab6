b01f3fe732298c0e373b11b7d24d204fec9edc11
rju <rju@purdue.edu>
2017-12-08 01:20:55 -0500

    init urls

---
+++ chat/urls.py
@@ -0,0 +1,8 @@
+# urls.py inside chat app
+
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+               url(r'^(?P<label>[\w-]{,50})/$', views.chat_room, name='chat_room'),
+               ]
