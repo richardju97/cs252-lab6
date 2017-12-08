28d5e107591c23bd8bff0f84109e74a3cdef120f
rju <rju@purdue.edu>
2017-12-08 01:50:15 -0500

    added routes

---
+++ Angular/script.js
@@ -0,0 +1,59 @@
+
+// script.js
+
+// Load Dependencies
+var app = angular.module('mainApp', ['ngRoute']);
+
+app.controller('mainController', function($scope) {} );
+
+app.controller('homeController', function($scope, $http) {
+               
+               $scope.name = "homeController";
+               });
+
+app.controller('drawController', function($scope, $http) {
+               
+               $scope.name = "drawController";
+               });
+
+app.controller('guessController', function($scope, $http) {
+               
+               $scope.name = "guessController";
+               });
+
+
+app.config(function($routeProvider, $locationProvider) {
+           
+           $routeProvider
+           
+           .when('/', {
+                 
+                 templateUrl: 'template/home.html',
+                 controller: 'homeController'
+                 })
+           
+           .when('/draw', {
+                 
+                 templateUrl: 'template/faq.html',
+                 controller: 'aboutController'
+                 })
+           
+           .when('/guess', {
+                 
+                 templateUrl: 'template/contact.html',
+                 controller: 'contactController'
+                 })
+           
+           .when('/error404', {
+                 
+                 templateUrl: 'template/404.html',
+                 controller: 'error404Controller'
+                 })
+           
+           .otherwise({
+                      
+                      redirectTo: '/'
+                      });
+           
+           $locationProvider.html5Mode(true);
+           });
