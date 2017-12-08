e4c698b9bd7f37359a9a6e90649fc01a33873485
rju <rju@purdue.edu>
2017-12-08 01:17:09 -0500

    starting chat views

---
+++ chat/views.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+def chat_room(request, label):
+    # If the room with the given label doesn't exist, automatically create it
+    # upon first visit (a la etherpad).
+    room, created = Room.objects.get_or_create(label=label)
+    
+    # We want to show the last 50 messages, ordered most-recent-last
+    messages = reversed(room.messages.order_by('-timestamp')[:50])
+    
+    return render(request, "chat/room.html", {
+                  'room': room,
+                  'messages': messages,
+                  })
